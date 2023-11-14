9939cb15aa8f319791174ad1eef72bba42b62451
Shane Mindreau <smindre1@binghamton.edu>
2023-11-13 21:08:26 -0500

    I set up all the model files. I created a mySQL database on workbench. Lastly, I seeded my database.

--- models/index.js
+++ models/index.js
@@ -4,13 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+
 // Products belongsTo Category
-
-// Categories have many Products
-
+Product.belongsTo(Category
+);
+// Categories has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: 'product_id'
+  }
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: 'tag_id'
+  }
+  
+})
 
 module.exports = {
   Product,
